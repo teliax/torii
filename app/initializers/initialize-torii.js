3b9522807d2c0a12a754f27c6a71803e7226ea7a
freddycashmercury <freddycashmercury@users.noreply.github.com>
2018-01-25 13:32:39 -0700

    Changes for pulling config from server.
    Modifies the torii initializer to pull in configuration from /config on the server it is hosted on successfully.

--- app/initializers/initialize-torii.js
+++ app/initializers/initialize-torii.js
@@ -5,16 +5,16 @@ import fetch from 'fetch';
 
 var initializer = {
   name: 'torii',
-  initialize: async function(application) {
+  initialize(application) {
     if (arguments[1]) { // Ember < 2.1
       application = arguments[1];
     }
-    let url = "https://ivy.teliax.dev:3000/config";
-    let response = await fetch(url);
-    let json = await response.json();
-
-    let config = json.configuration.ivy.development.config.frontend;
-    configure(config.torii || {});
+    let url = "/config";
+    let response = fetch(url_.then(response => {
+      response.json().then(json => {
+        configure(json.torii || {});
+      });
+    });
     bootstrapTorii(application);
     application.inject('route', 'torii', 'service:torii');
   }
