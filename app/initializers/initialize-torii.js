1d40775273ee9eff600daeadd4a184692cfcfd03
freddycashmercury <freddycashmercury@users.noreply.github.com>
2018-01-25 10:07:02 -0700

    Attempt at patching torii to work the way we want it
    Patches torii initializer to request config from remote url instead of from environment file.

--- app/initializers/initialize-torii.js
+++ app/initializers/initialize-torii.js
@@ -1,14 +1,19 @@
 import bootstrapTorii from 'torii/bootstrap/torii';
 import {configure} from 'torii/configuration';
-import config from '../config/environment';
+import fetch from 'fetch';
  
 
 var initializer = {
   name: 'torii',
-  initialize(application) {
+  initialize async function(application) {
     if (arguments[1]) { // Ember < 2.1
       application = arguments[1];
     }
+    let url = "https://ivy.teliax.dev:3000/config";
+    let response = await fetch(url);
+    let json = await response.json();
+
+    let config = json.configuration.ivy.development.config.frontend;
     configure(config.torii || {});
     bootstrapTorii(application);
     application.inject('route', 'torii', 'service:torii');
